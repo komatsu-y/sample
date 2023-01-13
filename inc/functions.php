7abf3bfe519caba501c48a8c7889809756319086
komatsu-y <komatsu.y.0923@gmail.com>
2023-01-13 12:02:42 +0900

    適当なコメント文

--- inc/functions.php
+++ inc/functions.php
@@ -2,14 +2,16 @@
 function str2html(string $string) :string {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
-function db_open() :PDO { //タイプヒントでPDO 型を指定
-    $user = "root";
-    $password = "1214"; //P172 で生成したパスワードを入力
+function db_open() :PDO {
+    $user     = 'koma09_komatsu';
+    $password = 'pass0111';
+    $host     = 'mysql1.php.xdomain.ne.jp' ;
+    $dbname   = 'koma09_shop';
     $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES => false,
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
     ];
-    $dbh = new PDO('mysql:host=localhost;dbname=sample_db', $user, $password, $opt);
-    return $dbh; //返り値を返す
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password, $opt);
+    return $dbh;
 }
